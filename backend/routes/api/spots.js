6e0d02a17a89848f2b32edd8c045ab767cf571b4
CoryCampbell <corycampbell20@yahoo.com>
2023-10-03 12:40:10 -0500

    spots started

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
-
+const { requireAuth } = require("../../utils/auth");
 const { Spot } = require("../../db/models");
 
 const router = express.Router();
 
-router.get("/current", async (req, res) => {
-    //authorize the current user
+router.get("/current", requireAuth, async (req, res) => {
     //return all spots owned by this user
+    const yourSpots = await Spot.findByPk(1);
+    res.json(yourSpots);
 });
 
 router.get("/", async (req, res) => {

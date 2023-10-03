2f63ea761c2ce0eed01eb21da2c560772403523b
CoryCampbell <corycampbell20@yahoo.com>
2023-10-03 12:49:05 -0500

    help2

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 
 const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
+const spotsRouter = require("./spots.js");
 
 const { setTokenCookie } = require("../../utils/auth.js");
-const { User } = require("../../db/models");
+const { User, Spot } = require("../../db/models");
 const { restoreUser, requireAuth } = require("../../utils/auth.js");
 
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
+router.use("/spots", spotsRouter);
 
 router.use("/users", usersRouter);
 

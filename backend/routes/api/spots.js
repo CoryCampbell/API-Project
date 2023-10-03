9f62b8efe2b9357b2273e24c7e9536f82aefbb0f
CoryCampbell <corycampbell20@yahoo.com>
2023-10-03 12:56:37 -0500

    idk

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,37 +1,34 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
+const { handleValidationErrors } = require("../../utils/validation");
 const { requireAuth } = require("../../utils/auth");
 const { Spot } = require("../../db/models");
-
-const { Op } = require("sequelize");
-const bcrypt = require("bcryptjs");
+const router = express.Router();
 
 const options = {};
-
-const router = express.Router();
 
 router.get("/", async (req, res) => {
     const allSpots = await Spot.findAll();
     res.status(200).json(allSpots);
 });
 
+// router.get("/current", requireAuth, async (req, res) => {
+//     const yourSpots = await Spot.findByPk();
+// });
+
+// router.get("/api/spots/:spotId", async (req, res) => {
+//     const { spotId } = req.params;
+
+//     const ownerSpots = await Spots.findByPk({
+//         where: {
+//             spotId
+//         }
+//     });
+// });
+
+// let spots = [];
+// let images = [];
+
+// router.post("/api/spots", async (req, res) => {});
+
 module.exports = router;
-
-router.get("/current", requireAuth, async (req, res) => {
-    const yourSpots = await Spot.findByPk();
-});
-
-router.get("/api/spots/:spotId", async (req, res) => {
-    const { spotId } = req.params;
-
-    const ownerSpots = await Spots.findByPk({
-        where: {
-            spotId
-        }
-    });
-});
-
-let spots = [];
-let images = [];
-
-router.post("/api/spots", async (req, res) => {});

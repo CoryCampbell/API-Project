4f0dbcef201ad046c51330362b57a022f61d6313
CoryCampbell <corycampbell20@yahoo.com>
2023-10-05 12:39:25 -0500

    options added

--- backend/db/migrations/20231003031442-create-spot.js
+++ backend/db/migrations/20231003031442-create-spot.js
@@ -1,67 +1,74 @@
 "use strict";
+
+let options = {};
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("Spots", {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            ownerId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Users",
-                    key: "id"
+        await queryInterface.createTable(
+            "Spots",
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER
+                },
+                ownerId: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "Users",
+                        key: "id"
+                    }
+                },
+                address: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false
+                },
+                city: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false
+                },
+                state: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false
+                },
+                country: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false
+                },
+                lat: {
+                    type: Sequelize.DECIMAL,
+                    allowNull: false
+                },
+                lng: {
+                    type: Sequelize.DECIMAL,
+                    allowNull: false
+                },
+                name: {
+                    type: Sequelize.STRING(50),
+                    allowNull: false
+                },
+                description: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false
+                },
+                price: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
                 }
             },
-            address: {
-                type: Sequelize.STRING(255),
-                allowNull: false
-            },
-            city: {
-                type: Sequelize.STRING(255),
-                allowNull: false
-            },
-            state: {
-                type: Sequelize.STRING(255),
-                allowNull: false
-            },
-            country: {
-                type: Sequelize.STRING(255),
-                allowNull: false
-            },
-            lat: {
-                type: Sequelize.DECIMAL,
-                allowNull: false
-            },
-            lng: {
-                type: Sequelize.DECIMAL,
-                allowNull: false
-            },
-            name: {
-                type: Sequelize.STRING(50),
-                allowNull: false
-            },
-            description: {
-                type: Sequelize.STRING(255),
-                allowNull: false
-            },
-            price: {
-                type: Sequelize.INTEGER,
-                allowNull: false
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
+            options
+        );
     },
     async down(queryInterface, Sequelize) {
         await queryInterface.dropTable("Spots");
--- backend/db/migrations/20231004201503-create-review.js
+++ backend/db/migrations/20231004201503-create-review.js
@@ -1,47 +1,54 @@
 "use strict";
+
+let options = {};
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        await queryInterface.createTable("Reviews", {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            userId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Users",
-                    key: "id"
+        await queryInterface.createTable(
+            "Reviews",
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER
+                },
+                userId: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "Users",
+                        key: "id"
+                    }
+                },
+                spotId: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    references: {
+                        model: "Users",
+                        key: "id"
+                    }
+                },
+                review: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                stars: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: Sequelize.DATE
                 }
             },
-            spotId: {
-                type: Sequelize.INTEGER,
-                allowNull: false,
-                references: {
-                    model: "Users",
-                    key: "id"
-                }
-            },
-            review: {
-                type: Sequelize.STRING,
-                allowNull: false
-            },
-            stars: {
-                type: Sequelize.INTEGER,
-                allowNull: false
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
+            options
+        );
     },
     async down(queryInterface, Sequelize) {
         await queryInterface.dropTable("Reviews");

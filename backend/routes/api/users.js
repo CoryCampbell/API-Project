d01e36a90a6bfc8eee903c2fea52dfea9c8180b9
CoryCampbell <corycampbell20@yahoo.com>
2023-10-05 15:02:33 -0500

    test

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -8,7 +8,7 @@ const { User } = require("../../db/models");
 const router = express.Router();
 
 const validateSignup = [
-    check("email").exists({ checkFalsy: true }).isEmail().withMessage("Invalid email"),
+    check("email").exists({ checkFalsy: true }).isEmail().withMessage("Please provide a valid email."),
     check("username")
         .exists({ checkFalsy: true })
         .isLength({ min: 4 })

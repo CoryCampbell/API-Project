4f4dd0c5d85d4b91a29d83a61f7e200febaa35b2
CoryCampbell <corycampbell20@yahoo.com>
2023-10-09 05:56:15 -0500

    try fix on errors

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -10,45 +10,6 @@ const { Review, ReviewImage, User, SpotImage, Spot } = require("../../db/models"
 
 const router = express.Router();
 
-// // Delete a Spot image
-// router.delete("/:imageId", requireAuth, async (req, res) => {
-//     const { imageId } = req.params;
-//     const { user } = req;
-
-//     const spotImageToDelete = await SpotImage.findByPK(imageId);
-
-//     console.log("spotImageToDelete", spotImageToDelete);
-
-//     if (!spotImageToDelete) {
-//         return res.status(404).json({
-//             "message": "Spot Image couldn't be found"
-//         });
-//     }
-
-//     try {
-//         const spotImageUserId = spotImageToDelete.Spot.ownerId;
-
-//         console.log("spotImageUserId", spotImageUserId);
-
-//         //authorization check
-//         // if (user.id === spotImageUserId) {
-//         //     await spotImage.destroy();
-
-//         //     res.json({
-//         //         "message": "Successfully deleted"
-//         //     });
-//         // } else {
-//         //     return res.status(403).json({
-//         //         "message": "Forbidden"
-//         //     });
-//         // }
-//         res.json("testing");
-//     } catch (error) {
-//         res.status(404).json({
-//             "message": "Spot Image couldn't be found"
-//         });
-//     }
-// });
 
 //DELETE A SPOT IMAGE
 router.delete("/:imageId", requireAuth, async (req, res) => {

4d47cbbfb7c7949e3bf22b50b621044573771c42
CoryCampbell <corycampbell20@yahoo.com>
2023-10-20 15:07:29 -0500

    signup css added

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -11,11 +11,11 @@ const setUser = (user) => {
     };
 };
 
-const removeUser = () => {
-    return {
-        type: REMOVE_USER
-    };
-};
+// const removeUser = () => {
+//     return {
+//         type: REMOVE_USER
+//     };
+// };
 
 export const login = (user) => async (dispatch) => {
     const { credential, password } = user;
@@ -38,11 +38,32 @@ export const restoreUser = () => async (dispatch) => {
     return response;
 };
 
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password
+        })
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
     let newState;
     switch (action.type) {
+        case RESTORE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
         case SET_USER:
             newState = Object.assign({}, state);
             newState.user = action.payload;

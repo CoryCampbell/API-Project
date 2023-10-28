fd0910b0723dc84c5e40e65938d7fa8067009015
CoryCampbell <corycampbell20@yahoo.com>
2023-10-28 16:34:15 -0500

    logout takes you to home page now

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -6,62 +7,63 @@ import "./LoginForm.css";
 
 function LoginFormModal() {
     const dispatch = useDispatch();
+
     const [credential, setCredential] = useState("");
     const [password, setPassword] = useState("");
     const [errors, setErrors] = useState({});
     const { closeModal } = useModal();
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors({});
-    return dispatch(sessionActions.login({ credential, password }))
-        .then(closeModal)
-        .catch(async (res) => {
-            const data = await res.json();
-            if (data && data.message) {
-                setErrors(data);
-            }
-        });
-};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErrors({});
+        return dispatch(sessionActions.login({ credential, password }))
+            .then(closeModal)
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.message) {
+                    setErrors(data);
+                }
+            });
+    };
 
-const demoUser = () => {
-    setCredential("DemoUserDownBad");
-    setPassword("password");
-};
+    const demoUser = () => {
+        setCredential("DemoUserDownBad");
+        setPassword("password");
+    };
 
-return (
-    <div className="loginContainer">
-        <h1 className="loginH1">Log In</h1>
-        <form onSubmit={handleSubmit} className="loginForm">
-            <label className="loginLabel">
-                Username or Email
-                <input
-                    className="loginInput"
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
-                />
-            </label>
-            <label className="loginLabel">
-                Password
-                <input
-                    className="loginInput"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            {errors.message && <p className="loginErrorMessage">{errors.message}</p>}
-            <button type="submit" className="loginButton">
-                Log In
-            </button>
-            <button className="demoUserButton" onClick={demoUser}>
-                Log in as Demo User
-            </button>
-        </form>
-    </div>
-);
+    return (
+        <div className="loginContainer">
+            <h1 className="loginH1">Log In</h1>
+            <form onSubmit={handleSubmit} className="loginForm">
+                <label className="loginLabel">
+                    Username or Email
+                    <input
+                        className="loginInput"
+                        type="text"
+                        value={credential}
+                        onChange={(e) => setCredential(e.target.value)}
+                        required
+                    />
+                </label>
+                <label className="loginLabel">
+                    Password
+                    <input
+                        className="loginInput"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                {errors.message && <p className="loginErrorMessage">{errors.message}</p>}
+                <button type="submit" className="loginButton">
+                    Log In
+                </button>
+                <button className="demoUserButton" onClick={demoUser}>
+                    Log in as Demo User
+                </button>
+            </form>
+        </div>
+    );
 }
 export default LoginFormModal;
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import * as sessionActions from "../../store/session";
 import OpenModalMenuItem from "./OpenModalMenuItem";
@@ -6,6 +7,7 @@ import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
 
 function ProfileButton({ user }) {
+    const history = useHistory();
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
     const ulRef = useRef();
@@ -35,6 +37,7 @@ function ProfileButton({ user }) {
         e.preventDefault();
         dispatch(sessionActions.logout());
         closeMenu();
+        history.push("/");
     };
 
     const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
